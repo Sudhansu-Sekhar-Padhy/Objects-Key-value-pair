ff5cf93304b5ffba1968af108edc860925f8c1f6
Sudhansu-Sekhar-Padhy <sidsekhar809@gmail.com>
2022-10-15 10:57:07 +0530

    key value pair

---
+++ Key value pair.js
@@ -0,0 +1,8 @@
+let students ={
+    rollno: 10,
+    name: "Arjun",
+    age: 18,
+    city: 'punjab',
+}
+console.log(students);//{rollno:10, name:'arjun', age:18, city:'punjab'}
+console.log(students.rollno);// 10
